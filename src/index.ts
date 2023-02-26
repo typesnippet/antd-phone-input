38461a57773f0fa40d923a08ea6450190d614436
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-02-26 20:26:35 +0400

    GH-6: Fix the loading of css

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import PhoneNumberFormItem from "./components/PhoneNumberFormItem";
 import PhoneNumberInput from "./components/PhoneNumberInput";
 
+import "react-phone-input-2/lib/style.css";
 import "./index.less";
 
 export {
