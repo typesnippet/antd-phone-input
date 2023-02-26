7bec90070a65c6e4cebc2118d02acb4ec0157e1d
Artyom Vancyan <44609997+ArtyomVancyan@users.noreply.github.com>
2023-02-26 20:47:48 +0400

    GH-6: Fix the style loading issue (GH-7)
    * Fix the loading of css
    * Fix the style issues for jest

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,8 @@
 import dts from "rollup-plugin-dts";
 import less from "rollup-plugin-less";
 import json from "@rollup/plugin-json";
+import postcss from "rollup-plugin-postcss";
+import resolve from "rollup-plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import {readFileSync} from "fs";
 
@@ -12,7 +14,9 @@ const esmOutput = {file: pkg.module, format: "es"};
 const dtsOutput = {file: pkg.types, format: "es"};
 
 const jsonPlugin = json();
+const cssPlugin = postcss();
 const tsPlugin = typescript();
+const resolvePlugin = resolve();
 const lessPlugin = less({insert: true, output: false});
 
 const external = [
@@ -22,7 +26,7 @@ const external = [
 ];
 
 export default [
-	{input, output: cjsOutput, plugins: [tsPlugin, jsonPlugin, lessPlugin], external},
-	{input, output: esmOutput, plugins: [tsPlugin, jsonPlugin, lessPlugin], external},
-	{input, output: dtsOutput, plugins: [dts()], external: [/\.less$/]},
+	{input, output: cjsOutput, plugins: [tsPlugin, jsonPlugin, resolvePlugin, cssPlugin], external},
+	{input, output: esmOutput, plugins: [tsPlugin, jsonPlugin, resolvePlugin, cssPlugin], external},
+	{input, output: dtsOutput, plugins: [dts(), lessPlugin], external: [/\.(?:le|c)ss$/]},
 ];
