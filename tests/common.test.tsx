8463802b94ad6fa2ecc71858f5ab00ea2c7f1c23
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-02 23:31:15 +0400

    Add two more tests for functionality check

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -1,5 +1,9 @@
 import assert from "assert";
-import {render} from "@testing-library/react";
+import Form from "antd/lib/form";
+import Button from "antd/lib/button";
+import FormItem from "antd/lib/form/FormItem";
+import userEvent from "@testing-library/user-event";
+import {render, screen} from "@testing-library/react";
 
 import PhoneInput from "../src";
 
@@ -17,7 +21,7 @@ Object.defineProperty(window, "matchMedia", {
 	})),
 })
 
-describe("<PhoneInput {...props}/>", () => {
+describe("Checks the basic rendering and functionality", () => {
 	it("Renders without crashing", () => {
 		render(<PhoneInput/>);
 	})
@@ -32,5 +36,35 @@ describe("<PhoneInput {...props}/>", () => {
 			}}
 			value={{countryCode: 1, areaCode: 702, phoneNumber: "1234567"}}
 		/>);
+	})
+
+	it("Checks the component on user input", async () => {
+		render(<PhoneInput
+			onChange={value => {
+				assert(value.isoCode === "us");
+			}}
+			country="us"
+		/>);
+		const input = screen.getByDisplayValue("+1");
+		await userEvent.type(input, "702123456789");
+		assert(input.getAttribute("value") === "+1 (702) 123 4567");
+	})
+
+	it("Uses the input with FormItem", async () => {
+		render(<Form onFinish={({phone}) => {
+			assert(phone.countryCode === 1);
+			assert(phone.areaCode === 702);
+			assert(phone.phoneNumber === "1234567");
+			assert(phone.isoCode === "us");
+		}}>
+			<FormItem name="phone">
+				<PhoneInput country="us"/>
+			</FormItem>
+			<Button data-testid="button" htmlType="submit">Submit</Button>
+		</Form>);
+		const input = screen.getByDisplayValue("+1");
+		await userEvent.type(input, "702123456789");
+		assert(input.getAttribute("value") === "+1 (702) 123 4567");
+		screen.getByTestId("button").click();
 	})
 })
