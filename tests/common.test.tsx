a784d4af3722419e4f2cde370f62c3c8babda2cc
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-07 14:17:12 +0400

    GH-19: Add tests for input validation

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -33,9 +33,11 @@ describe("Checks the basic rendering and functionality", () => {
 				assert(value.areaCode === 702);
 				assert(value.phoneNumber === "1234567");
 				assert(value.isoCode === "us");
+				assert(value.valid === true);
 			}}
 			value={{countryCode: 1, areaCode: 702, phoneNumber: "1234567"}}
 		/>);
+		assert(screen.getByDisplayValue("+1 (702) 123 4567"));
 	})
 
 	it("Checks the component on user input", async () => {
@@ -66,5 +68,20 @@ describe("Checks the basic rendering and functionality", () => {
 		await userEvent.type(input, "702123456789");
 		assert(input.getAttribute("value") === "+1 (702) 123 4567");
 		screen.getByTestId("button").click();
+	})
+
+	it("Checks input validation with FormItem", async () => {
+		render(<Form initialValues={{phone: {countryCode: 1, areaCode: 702, phoneNumber: "1234567"}}}>
+			<FormItem name="phone" rules={[{
+				validator: (_, {valid}) => {
+					assert(valid === true);
+					return Promise.resolve();
+				}
+			}]}>
+				<PhoneInput/>
+			</FormItem>
+			<Button data-testid="button" htmlType="submit">Submit</Button>
+		</Form>);
+		await userEvent.click(screen.getByTestId("button"));
 	})
 })
