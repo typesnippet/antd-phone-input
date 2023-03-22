475cc09645d3a9d7902a8486967c98fec4e5a137
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-22 22:39:22 +0400

    GH-20: Run the tests for both versions of antd

---
+++ tests/antd4.test.tsx
@@ -0,0 +1,8 @@
+import Form from "antd4/lib/form";
+import Button from "antd4/lib/button";
+import FormItem from "antd4/lib/form/FormItem";
+
+import commonTests from "./common";
+import PhoneInput from "../src/index4";
+
+commonTests(PhoneInput, Form, FormItem, Button);
---
+++ tests/antd5.test.tsx
@@ -0,0 +1,8 @@
+import Form from "antd/lib/form";
+import Button from "antd/lib/button";
+import FormItem from "antd/lib/form/FormItem";
+
+import commonTests from "./common";
+import PhoneInput from "../src/index5";
+
+commonTests(PhoneInput, Form, FormItem, Button);
