36436f5a0550c896a23ef80b1df4b3499ed9212c
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-03-27 15:25:44 +0400

    GH-29: Inject the constant CSS rules in the rollup build

--- rollup.config.ts
+++ rollup.config.ts
@@ -16,8 +16,16 @@ const cjsInput5 = {file: "index.cjs.js", format: "cjs", exports: "auto"};
 const esmInput5 = {file: "index.esm.js", format: "es"};
 const dtsInput5 = {file: "index.d.ts", format: "es"};
 
+const stringifier = (root: any, builder: any) => {
+	/** Include the CSS rules describing the flags after the main styles */
+	const style = readFileSync("node_modules/react-phone-input-2/lib/style.css");
+	const regex = /(\.react-tel-input)\s\.flag.+\1\s*\.\w{2}\b\s*\{[^{}]*}/;
+	const rules: any = style.toString().match(regex);
+	builder(root.toString() + "\n" + (rules ? rules[0] : ""));
+}
+
 const jsonPlugin = json();
-const cssPlugin = postcss();
+const cssPlugin = postcss({stringifier});
 const tsPlugin = typescript();
 const aliasPlugin = alias({entries: {"antd/lib": "antd/es"}});
 
