8b076b6fb3b7b5cb27e718c7624a0e944c0ca2c0
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-09-19 15:36:35 +0400

    Load flag stylesheets from `react-phone-input-2`
    Signed-off-by: Artyom Vancyan <artyom.vancyan2000@gmail.com>

--- scripts/prepare-styles.ts
+++ scripts/prepare-styles.ts
@@ -7,9 +7,14 @@ const exec = (command: string) => util.promisify(process.exec)(command);
 (async () => {
 	await exec("bash -c 'cp src/legacy/*.{le,c}ss legacy'");
 
-	let styles = fs.readFileSync("./legacy/style5.css", "utf8");
+	const style = fs.readFileSync("node_modules/react-phone-input-2/lib/style.css");
+	const regex = /(\.react-tel-input)\s\.flag.+\1\s*\.\w{2}\b\s*\{[^{}]*}/;
+	const flags = "\n" + (style.toString().match(regex) || [""])[0];
+
+	let styles = fs.readFileSync("./legacy/style5.css", "utf8") + flags;
 	styles = styles.replaceAll(/\/\*\*.*?\*\/[\n\s]*/gs, "");
 	styles = styles.replaceAll(/\B[^{}]*?\{[\s\n]}/g, "");
+	styles = styles.replaceAll(/\//g, "\\\/");
 	styles = styles.replaceAll(/\n\s*/g, "");
 
 	await exec(`find legacy -maxdepth 1 -name '*.js' -type f -exec sed -i 's/style5.css/${styles}/g' {} +`);
