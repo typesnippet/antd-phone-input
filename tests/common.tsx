e5e75507518db2be6a7a5af4d6731647161a1e10
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-09-21 23:11:35 +0400

    GH-40: Add test cases for the new validation fixes
    Signed-off-by: Artyom Vancyan <artyom.vancyan2000@gmail.com>

--- tests/common.tsx
+++ tests/common.tsx
@@ -1,6 +1,6 @@
 import assert from "assert";
 import userEvent from "@testing-library/user-event";
-import {render, screen} from "@testing-library/react";
+import {act, render, screen} from "@testing-library/react";
 
 Object.defineProperty(window, "matchMedia", {
 	writable: true,
@@ -17,6 +17,8 @@ Object.defineProperty(window, "matchMedia", {
 })
 
 export default function commonTests(PhoneInput: any, Form: any, FormItem: any, Button: any) {
+	console.warn = jest.fn();
+
 	describe("Checking the basic rendering and functionality", () => {
 		it("Rendering without crashing", () => {
 			render(<PhoneInput/>);
@@ -29,7 +31,7 @@ export default function commonTests(PhoneInput: any, Form: any, FormItem: any, B
 					assert(value.areaCode === 702);
 					assert(value.phoneNumber === "1234567");
 					assert(value.isoCode === "us");
-					assert(value.valid() === true);
+					assert(value.valid());
 				}}
 				value={{countryCode: 1, areaCode: 702, phoneNumber: "1234567"}}
 			/>);
@@ -70,7 +72,7 @@ export default function commonTests(PhoneInput: any, Form: any, FormItem: any, B
 			render(<Form initialValues={{phone: {countryCode: 1, areaCode: 702, phoneNumber: "1234567"}}}>
 				<FormItem name="phone" rules={[{
 					validator: (_: any, {valid}: any) => {
-						assert(valid() === true);
+						assert(valid());
 						return Promise.resolve();
 					}
 				}]}>
@@ -91,6 +93,96 @@ export default function commonTests(PhoneInput: any, Form: any, FormItem: any, B
 			const input = screen.getByDisplayValue("+1 (702)");
 			await userEvent.type(input, "1234567");
 			assert(input.getAttribute("value") === "+1 (702) 123 4567");
+		})
+
+		it("Checking validation with casual form actions", async () => {
+			render(<Form data-testid="form" initialValues={{phone: {countryCode: 1, areaCode: 702, phoneNumber: ""}}}>
+				<FormItem name="phone" rules={[{
+					validator: (_: any, {valid}: any) => {
+						if (valid()) return Promise.resolve();
+						return Promise.reject("Invalid phone number");
+					}
+				}]}>
+					<PhoneInput/>
+				</FormItem>
+				<Button data-testid="submit" htmlType="submit">Submit</Button>
+				<Button data-testid="reset" htmlType="reset">Reset</Button>
+			</Form>);
+
+			const form = screen.getByTestId("form");
+			const input = screen.getByDisplayValue("+1 (702)");
+			const submit = screen.getByTestId("submit");
+			const reset = screen.getByTestId("reset");
+
+			await userEvent.click(reset);
+			assert(form.querySelector(".ant-form-item-has-success") !== null); // valid
+			await userEvent.type(input, "a");
+			await userEvent.click(submit);
+			await act(async () => {
+				await new Promise(r => setTimeout(r, 100));
+			})
+			assert(form.querySelector(".ant-form-item-has-error") !== null); // invalid
+			await userEvent.click(reset);
+			assert(form.querySelector(".ant-form-item-has-success") !== null); // valid
+			await userEvent.click(reset);
+			assert(form.querySelector(".ant-form-item-has-success") !== null); // valid
+			await userEvent.type(input, "a");
+			await userEvent.click(submit);
+			await act(async () => {
+				await new Promise(r => setTimeout(r, 100));
+			})
+			assert(form.querySelector(".ant-form-item-has-error") !== null); // invalid
+			await userEvent.click(submit);
+			await act(async () => {
+				await new Promise(r => setTimeout(r, 100));
+			})
+			assert(form.querySelector(".ant-form-item-has-error") !== null); // invalid
+		})
+
+		it("Checking validation with casual inputs and actions", async () => {
+			render(<Form data-testid="form">
+				<FormItem name="phone" rules={[{
+					validator: (_: any, {valid}: any) => {
+						if (valid()) return Promise.resolve();
+						return Promise.reject("Invalid phone number");
+					}
+				}]}>
+					<PhoneInput country="us"/>
+				</FormItem>
+				<Button data-testid="submit" htmlType="submit">Submit</Button>
+				<Button data-testid="reset" htmlType="reset">Reset</Button>
+			</Form>);
+
+			const form = screen.getByTestId("form");
+			const input = screen.getByDisplayValue("+1");
+			const submit = screen.getByTestId("submit");
+			const reset = screen.getByTestId("reset");
+
+			await userEvent.type(input, "70212345");
+			await act(async () => {
+				await new Promise(r => setTimeout(r, 100));
+			})
+			assert(form.querySelector(".ant-form-item-has-error") !== null); // invalid
+			await userEvent.type(input, "6");
+			await act(async () => {
+				await new Promise(r => setTimeout(r, 100));
+			})
+			assert(form.querySelector(".ant-form-item-has-error") !== null); // invalid
+			await userEvent.type(input, "7");
+			await act(async () => {
+				await new Promise(r => setTimeout(r, 100));
+			})
+			assert(form.querySelector(".ant-form-item-has-success") !== null); // valid
+			await userEvent.click(reset);
+			assert(form.querySelector(".ant-form-item-has-success") !== null); // valid
+			await userEvent.type(input, "a");
+			await userEvent.click(submit);
+			await act(async () => {
+				await new Promise(r => setTimeout(r, 100));
+			})
+			assert(form.querySelector(".ant-form-item-has-error") !== null); // invalid
+			await userEvent.click(reset);
+			assert(form.querySelector(".ant-form-item-has-success") !== null); // valid
 		})
 	})
 }
