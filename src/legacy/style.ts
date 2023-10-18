8ce17d7ea888a76f533cbfe6cdbe624a61573e74
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-10-18 18:39:52 +0400

    GH-53: Get rid of `react-phone-input-2` related styles
    Signed-off-by: Artyom Vancyan <artyom.vancyan2000@gmail.com>

--- src/legacy/style.ts
+++ src/legacy/style.ts
@@ -1,19 +1,4 @@
 export default (cssText: string) => {
-	/** Resolve the overlap issue of the CSS rules when using Bootstrap */
-	for (const styleSheet of Array.from(document.styleSheets)) {
-		try {
-			for (const rule of Array.from(styleSheet.cssRules || styleSheet.rules)) {
-				if (rule instanceof CSSStyleRule) {
-					rule.selectorText = rule.selectorText.replace(
-						/^\.form-control(?=:|$)/,
-						".form-control:not(.ant-input)",
-					)
-				}
-			}
-		} catch (e) {
-		}
-	}
-
 	/** Inject the given `cssText` in the document head */
 	const style = document.createElement("style");
 	style.setAttribute("type", "text/css");
