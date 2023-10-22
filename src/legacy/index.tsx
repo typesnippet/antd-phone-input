881da3c9b5e7048a9460582cfc5119f16eb2e181
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-10-22 20:17:56 +0400

    GH-53: Implement the input mask formatting
    Signed-off-by: Artyom Vancyan <artyom.vancyan2000@gmail.com>

--- src/legacy/index.tsx
+++ src/legacy/index.tsx
@@ -1,29 +1,23 @@
-import {useMemo, useState} from "react";
-import Select from "antd/es/select";
-import Input from "antd/es/input";
+import {useCallback, useMemo, useState} from "react";
+import Select from "antd/lib/select";
+import Input from "antd/lib/input";
 
-import {PhoneInputProps, PhoneNumber} from "../types";
-
-// import masks from "./phoneMasks.json";
+import styleInject from "./style";
 import timezones from "./timezones.json";
 import countries from "./countries.json";
-// import validations from "./validations.json";
-import "./style5.css";
+import {PhoneInputProps} from "../types";
 
-// type ISO2Code = keyof typeof masks;
-type Timezone = keyof typeof timezones;
+styleInject("style5.css");
 
-const format = (value: PhoneNumber | string) => {
-	if (typeof value !== "string") {
-		const {countryCode, areaCode, phoneNumber} = {...value};
-		value = [countryCode, areaCode, phoneNumber].filter(Boolean).join("");
-	}
-	return value.replace(/^(?=\d)/, "+");
+const slots = new Set(".");
+
+const displayFormat = (value: string) => {
+	return value.replace(/[.\s\D]+$/, "").replace(/(\(\d+)$/, "$1)");
 }
 
 const getDefaultISO2Code = () => {
 	/** Returns the default ISO2 code, based on the user's timezone */
-	return (timezones[Intl.DateTimeFormat().resolvedOptions().timeZone as Timezone] || "") || "us";
+	return (timezones[Intl.DateTimeFormat().resolvedOptions().timeZone as keyof typeof timezones] || "") || "us";
 }
 
 // const parsePhoneNumber = (value: string, data: CountryData, formattedNumber: string): PhoneNumber => {
@@ -54,7 +48,7 @@ const getDefaultISO2Code = () => {
 // }
 
 const PhoneInput = ({
-						value,
+						value: defaultValue,
 						// style,
 						country,
 						// className,
@@ -66,17 +60,43 @@ const PhoneInput = ({
 						// ...reactPhoneInputProps
 					}: PhoneInputProps) => {
 	const defaultCountry = country || getDefaultISO2Code();
-	const defaultDialCode = countries.find(([isoCode]) => isoCode === defaultCountry)?.[1];
+	const defaultDialCode = countries.find(([isoCode]) => isoCode === defaultCountry)?.[3];
 
+	let back = false;
+	const [value, setValue] = useState<string>(defaultValue as string);
 	const [minWidth, setMinWidth] = useState(0);
-	const [rawValue, setRawValue] = useState(format(value as any));
 	const [dialCode, setDialCode] = useState(defaultDialCode);
+	const [pattern, setPattern] = useState("+. (...) ... ....");
 
-	const countryCode = useMemo(() => {
-		return countries.find(([_, dial]) => dialCode === dial)?.[0];
-	}, [dialCode])
+	const prev = useMemo(() => (j => Array.from(pattern, (c, i) => slots.has(c) ? j = i + 1 : j))(0), [pattern])
 
-	console.log(countryCode);
+	const first = useMemo(() => [...pattern].findIndex(c => slots.has(c)), [pattern])
+
+	const clean = useCallback((input: any) => {
+		input = input.match(/\d/g) || [];
+		return Array.from(pattern, c => input[0] === c || slots.has(c) ? input.shift() || c : c);
+	}, [pattern])
+
+	const onBlur = useCallback(({target}: any) => target.value === pattern && setValue(""), [pattern])
+
+	const onKeyDown = (e: any) => back = e.key === "Backspace"
+
+	const format = ({target}: any) => {
+		const [i, j] = [target.selectionStart, target.selectionEnd].map((i: any) => {
+			i = clean(target.value.slice(0, i)).findIndex(c => slots.has(c));
+			return i < 0 ? prev[prev.length - 1] : back ? prev[i - 1] || first : i;
+		});
+		target.value = displayFormat(clean(target.value).join(""));
+		setValue(target.value);
+		target.setSelectionRange(i, j);
+		back = false;
+	}
+
+	// const countryCode = useMemo(() => {
+	// 	return countries.find(([_, dial]) => dialCode === dial)?.[0];
+	// }, [dialCode])
+	//
+	// console.log(countryCode);
 
 	// const checkValidity = (metadata: PhoneNumber) => {
 	// 	// TODO: Update masks - create a script for preparing the masks (like it is done for validations)
@@ -119,18 +139,22 @@ const PhoneInput = ({
 		<Select
 			value={dialCode}
 			suffixIcon={null}
-			onSelect={setDialCode}
+			onSelect={(dial, {key: mask}) => {
+				setDialCode(dial);
+				setPattern(mask);
+			}}
 			optionLabelProp="label"
 			dropdownStyle={{minWidth}}
 		>
-			{countries.map(([isoCode, dialCode, countryName]) => (
+			{countries.map(([isoCode, countryName, _, dialCode, mask]) => (
 				<Select.Option
-					key={dialCode}
+					key={mask}
 					value={dialCode}
 					label={<div className={`flag ${isoCode}`}/>}
 					children={<div className="ant-phone-input-select-item">
 						<div className={`flag ${isoCode}`}/>
-						{countryName}&nbsp;+{dialCode}</div>}
+						{countryName}&nbsp;{displayFormat(mask)}
+					</div>}
 				/>
 			))}
 		</Select>
@@ -140,13 +164,19 @@ const PhoneInput = ({
 		<div ref={node => setMinWidth(node?.offsetWidth || 0)}>
 			<Input
 				/** Static properties for stable functionality */
-				value={rawValue}
+				inputMode="tel"
+				value={value}
+				onBlur={onBlur}
+				onInput={format}
+				onFocus={format}
+				onKeyDown={onKeyDown}
 				addonBefore={countriesSelect}
 				/** Static properties providing dynamic behavior */
 				// onMount={onMount}
-				onChange={(e) => {
-					setRawValue(format(e.target.value));
-				}}
+				// onChange={(e) => {
+				// 	console.log(e.target.value);
+				// 	setValue(e.target.value);
+				// }}
 				// country={countryCode}
 				// inputClass={inputClass}
 				/** Dynamic properties for customization */
