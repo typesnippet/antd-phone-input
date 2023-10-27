1e40a57b665e9ddff620733b410db1ffe87f0c79
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-10-27 14:12:58 +0400

    GH-53: Load styles from JSON in simplified way
    Signed-off-by: Artyom Vancyan <artyom.vancyan2000@gmail.com>

--- scripts/prepare-styles.ts
+++ scripts/prepare-styles.ts
@@ -1,15 +1,16 @@
-import fs from "fs";
 import util from "node:util";
 import process from "node:child_process";
+
+import stylesheet from "../resources/stylesheet.json";
 
 const exec = (command: string) => util.promisify(process.exec)(command, {shell: "/bin/bash"});
 
 (async () => {
-	let styles = fs.readFileSync("./src/styles.css", "utf8");
-	styles = styles.replaceAll(/\/\*\*.*?\*\/[\n\s]*/gs, "");
-	styles = styles.replaceAll(/\B[^{}]*?\{[\s\n]}/g, "");
-	styles = styles.replaceAll(/\//g, "\\\/");
-	styles = styles.replaceAll(/\n\s*/g, "");
+	let styles = Object.entries(stylesheet).map(([selector, rules]) => {
+		return `${selector} {` + Object.entries(rules).map(([key, value]) => {
+			return `${key}: ${value}; `;
+		}).join("") + "} ";
+	}).join("");
 
 	await exec(`ls *.js | xargs -I {} sed -i 's/styles.css/${styles}/g' {}`);
 	await exec("ls *.{j,t}s | xargs -I {} sed -i 's/antd\\/lib/antd\\/es/g' {}");
