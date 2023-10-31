2f91a29ac141435136de671bc3cde5cff3a3bb6a
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-10-31 12:14:18 +0400

    GH-53: Complete mask regenerator implementation
    Signed-off-by: Artyom Vancyan <artyom.vancyan2000@gmail.com>

--- scripts/prepare-metadata/main.py
+++ scripts/prepare-metadata/main.py
@@ -12,17 +12,13 @@ territories = tree.find("territories")
 
 
 def update_mask(mask, length):
-    """
-    update slots of the mask to match the given length
-
-    e.g., update_mask("+1 (664) ....", 10) -> "+1 (664) .... ..."
-    """
     length_without_cc = len(re.findall(r"[\d.]", re.sub(r"\+\d+\s", "", mask)))
     if length_without_cc != length:
-        ac_mask = re.match(r"\+\d+\s(\S+)\s", mask).group(1)
+        cc_mask = re.match(r"(\+\d+)\s", mask).group(1)
+        ac_mask = re.match(r"\+\d+\s(?:(\S+)\s)?", mask).group(1) or ""
         ac_mask_length = len(re.findall(r"[\d.]", ac_mask))
         pn_mask_expected_length = length - ac_mask_length
-        pn_mask_prefix = re.sub(r"\+\d+\s\S+\s", "", mask).split()[0]
+        pn_mask_prefix = re.sub(r"\+\d+\s(?:\S+\s)?", "", mask).split()[0]
         pn_mask_parts = [pn_mask_prefix]
         while len("".join(pn_mask_parts)) < pn_mask_expected_length:
             pn_mask_parts.append("." * len(pn_mask_prefix))
@@ -30,24 +26,19 @@ def update_mask(mask, length):
         while len(pn_mask.replace(" ", "")) > pn_mask_expected_length:
             pn_mask = pn_mask[:-1]
         assert len(re.findall(r"[\d.]", re.sub(r"\+\d+\s", "", f"{ac_mask} {pn_mask}"))) == length
-        return f"{ac_mask} {pn_mask}"
-    return mask
+        return re.sub(r"\s(\.{1,2})$", r"\1", f"{cc_mask} {ac_mask} {pn_mask}" if ac_mask else f"{cc_mask} {pn_mask}")
+    return re.sub(r"\s(\.{1,2})$", r"\1", mask)
 
 
 with open(countries_path) as fp:
     countries = json.load(fp)
 
 for territory in territories:
-    # if territory.get("id") != "MS":
-    #     continue
     possibleLengths = map(lambda e: territory.find(f"{e.tag}/possibleLengths"), territory.iter())
     possibleLengths = map(lambda e: e.get("national"), filter(lambda e: e is not None, possibleLengths))
     possibleLengths = list(map(int, re.findall(r"\d+", ",".join(possibleLengths))))
-    # todo: (re)generate phone masks and basic regexes for checking only the length (non-strict validation)
-    # print(min(possibleLengths), max(possibleLengths))
     for country in [c for c in countries if c[0] == territory.get("id").lower()]:
-        mask = country[4]
-        # print(mask, max(possibleLengths))
-        print(update_mask(mask, max(possibleLengths)))
-        # pn_mask = re.sub(r"\+\d+\s\S+\s", "", mask)
-        # print(pn_mask.split()[0])
+        country[4] = update_mask(country[4], max(possibleLengths))
+
+with open(countries_path, "w") as fp:
+    json.dump(countries, fp, indent=2)
