cd8141688ba8ea482a94d6a797715abcf9bb769e
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-11-05 14:55:02 +0400

    GH-53: Remove version specific initiations and merge tests
    Signed-off-by: Artyom Vancyan <artyom.vancyan2000@gmail.com>

--- tests/antd4.test.tsx
+++
@@ -1,7 +0,0 @@
-import Form from "antd4/lib/form";
-import Button from "antd4/lib/button";
-import FormItem from "antd4/lib/form/FormItem";
-
-import commonTests from "./common";
-
-commonTests(Form, FormItem, Button);
--- tests/antd5.test.tsx
+++
@@ -1,7 +0,0 @@
-import Form from "antd/lib/form";
-import Button from "antd/lib/button";
-import FormItem from "antd/lib/form/FormItem";
-
-import commonTests from "./common";
-
-commonTests(Form, FormItem, Button);
